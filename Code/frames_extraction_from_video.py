14898b6c9aadb8febe5da576b5dae3171ef76a2b
Koushik Roy <rkoushikroy2@gmail.com>
2025-04-24 14:11:10 -0500

    frame extraction done

--- Code/frames_extraction_from_video.py
+++ Code/frames_extraction_from_video.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 # === User-configurable paths ===
 raw_dataset_path   = r'C:\Users\rkous\Downloads\Compressed\1188976'         # folder containing Video_Speech_Actor_XX.zip
-output_dataset_path = r'C:\Users\rkous\OneDrive - Oklahoma A and M System\courses\Spring 25\Deep Learning\Projects\final_project\dataset'  # root for train/val/test folders
+output_dataset_path = r'C:\Users\rkous\OneDrive - Oklahoma A and M System\courses\Spring 25\Deep Learning\Projects\final_project\Final-Project-10\dataset'  # root for train/val/test folders
 
 # === Emotion code → name map ===
 emotion_map = {
